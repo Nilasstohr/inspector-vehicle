28a6037dbceb1a0f379266a8c233e10ecaed3849
Robot1 <Nilasstohr@hotmail.com>
2022-08-21 20:47:41 +0200

    added new dev lauch file for pycharm and added working python laserscan subcriper in robotvehicle monitor package

---
+++ ros2_ws/StartDevEnvironmentPyCharm.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# example of using arguments to a script
+source install/setup.bash
+sudo chmod 666 /dev/ttyUSB0
+../../../pycharm-community-2022.2.1/bin/pycharm.sh
+#echo $(pwd)   
+
---
+++ ros2_ws/src/robotvehicle_monitor/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'robotvehicle_monitor'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='robot1',
+    maintainer_email='Nilasstohr@hotmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        	'laserscan = robotvehicle_monitor.subscriber_laserscan:main'
+        ],
+    },
+)
