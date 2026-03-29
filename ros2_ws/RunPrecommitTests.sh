92d959ac76d0a9feb68466fdedfc59834716dd81
Nilas <Nilasstohr@hotmail.com>
2026-03-29 16:54:23 +0200

    0013: Added deploy to release

--- ros2_ws/RunPrecommitTests.sh
+++ ros2_ws/RunPrecommitTests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 source /opt/ros/humble/setup.bash
-colcon test --packages-select robotvehicle_package --ctest-args -R "TestNavigationUnitTest|LineDetectionIntegrationTest|ControllerNodeIntegrationTest"
+colcon test --packages-select robotvehicle_package --ctest-args -R "TestNavigationUnitTest|LineDetectionIntegrationTest"
 colcon test-result --all --verbose
